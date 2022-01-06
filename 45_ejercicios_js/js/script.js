54a8992504cec26022c55907365dcd35720a0647
Gilberto Vicente <vicenteyoc15@gmail.com>
2022-01-06 13:05:25 -0600

    solucion ejercicio 3

--- 45_ejercicios_js/js/script.js
+++ 45_ejercicios_js/js/script.js
@@ -1,9 +1,15 @@
 
-const saludo = document.querySelector("#boton")
-console.log(saludo)
+const boton = document.querySelector("#boton");
+const color = document.querySelector("#texto");
+const normal = document.querySelector("#regresar");
+
+function texto(){
+    color.style.color = 'green';
+}
+function regresar(){
+    color.style.color = 'black';
+}
 
 
-const saludar = () => {
-    
-};
-saludo.addEventListener("click", saludar);
+boton.addEventListener('click', texto);
+normal.addEventListener('click', regresar);
